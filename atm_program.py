b61187fdde780116c8d416bc49564073ecbdc61d
dh-atha <athadhaiffathin@gmail.com>
2020-09-17 17:19:59 +0700

    Update atm_program.py

---
+++ atm_program.py
@@ -0,0 +1,17 @@
+import random
+import datetime
+from customer import Customer
+
+atm = Customer(id)
+
+while True:
+    id = int(input("Masukkan pin anda: "))
+    trial = 0
+
+while (id != int(atm.checkPin()) and trial < 3):
+    id = int(input("Pin anda salah. Silakan Masukkan lagi: "))
+    trial += 1
+
+if trial == 3:
+    print("Error. Silakan ambil kartu dan coba lagi..")
+    exit()
